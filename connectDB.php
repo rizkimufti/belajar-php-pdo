58fc5fa708f5434b9368f0c9d18ae8e011c521cb
rizkimufti <rizkimufti@gmail.com>
2017-04-10 12:30:20 +0000

    DU pertemuan 4

---
+++ connectDB.php
@@ -0,0 +1,21 @@
+
+<?php
+
+// UNTUK MENGKONESIKAN KE SERVER
+$host = 'localhost';
+$username = 'root';
+$password = 'root';
+$database = 'blog';
+$dbms = 'mysql';
+$charset = 'utf8';
+$db = null;
+try {
+    $db = new PDO("$dbms:host=$host;dbname=$database;charset=$charset", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "koneksi lancar jaya";
+}
+    catch(PDOException $e) {
+    echo 'koneksi error :'.$e->getMessage();
+}
+
+?>
